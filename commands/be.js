3eca8bc93fea12a3512273c692cbdd6202c2cc2d
a31361548 <a31361548@gmail.com>
2023-12-22 16:07:04 +0800

    slice

--- commands/be.js
+++ commands/be.js
@@ -7,23 +7,25 @@ export default async (event) => {
     const { data } = await axios.get('https://www.sushiexpress.com.tw/sushi-express/Menu')
     const $ = cheerio.load(data)
     const replies = []
-    $('.grid-item').each(function () {
-      // 取出圖片和標題
-      const image = $(this).find('img').attr('src')
-      const imageUrl = new URL(image, 'https://www.sushiexpress.com.tw/').href
-      const product = $(this).find('.product_name').text().trim()
-      const productEn = $(this).find('.product_name_en').text()
-      const price = $(this).find('.product_name_jp').text()
-      // 產生一個新的回應訊息模板
-      const template = cardsTemplate()
-      // 修改模板內容
-      template.hero.url = imageUrl
-      template.body.contents[0].text = product
-      template.body.contents[1].contents[0].contents[0].text = productEn
-      template.body.contents[1].contents[1].contents[0].text = '價格(Price)'
-      template.body.contents[1].contents[1].contents[1].text = price
-      replies.push(template)
-    })
+    $('.grid-item')
+      .slice(0, 12)
+      .each(function () {
+        // 取出圖片和標題
+        const image = $(this).find('img').attr('src')
+        const imageUrl = new URL(image, 'https://www.sushiexpress.com.tw/').href
+        const product = $(this).find('.product_name').text().trim()
+        const productEn = $(this).find('.product_name_en').text()
+        const price = $(this).find('.product_name_jp').text()
+        // 產生一個新的回應訊息模板
+        const template = cardsTemplate()
+        // 修改模板內容
+        template.hero.url = imageUrl
+        template.body.contents[0].text = product
+        template.body.contents[1].contents[0].contents[0].text = productEn
+        template.body.contents[1].contents[1].contents[0].text = '價格(Price)'
+        template.body.contents[1].contents[1].contents[1].text = price
+        replies.push(template)
+      })
 
     const result = await event.reply({
       type: 'flex',
